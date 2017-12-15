be847ea74ef08548ece74e8d5dda14fd202b9110
MarvellousCat <neko@neko.home>
2017-12-15 13:57:05 +0100

    bugfix(treatment)

--- src/app/doctor-space/patient-treatment/patient-treatment.component.ts
+++ src/app/doctor-space/patient-treatment/patient-treatment.component.ts
@@ -233,21 +233,25 @@ export class PatientTreatmentComponent implements OnInit {
   }
 
   removeAccent(string: string) {
-    const accent = [
-      /[\300-\306]/g, /[\340-\346]/g, // A, a
-      /[\310-\313]/g, /[\350-\353]/g, // E, e
-      /[\314-\317]/g, /[\354-\357]/g, // I, i
-      /[\322-\330]/g, /[\362-\370]/g, // O, o
-      /[\331-\334]/g, /[\371-\374]/g, // U, u
-      /[\321]/g, /[\361]/g, // N, n
-      /[\307]/g, /[\347]/g, // C, c
-    ];
-    const noaccent = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
-    let result = string;
-    for (let i = 0; i < accent.length; i++) {
-      result = result.replace(accent[i], noaccent[i]);
+    if (string == null) {
+      return null;
     }
-    return result;
+      const accent = [
+        /[\300-\306]/g, /[\340-\346]/g, // A, a
+        /[\310-\313]/g, /[\350-\353]/g, // E, e
+        /[\314-\317]/g, /[\354-\357]/g, // I, i
+        /[\322-\330]/g, /[\362-\370]/g, // O, o
+        /[\331-\334]/g, /[\371-\374]/g, // U, u
+        /[\321]/g, /[\361]/g, // N, n
+        /[\307]/g, /[\347]/g, // C, c
+      ];
+      const noaccent = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
+      let result = string;
+      for (let i = 0; i < accent.length; i++) {
+        result = result.replace(accent[i], noaccent[i]);
+      }
+      return result;
+
   }
 
   getInteraction(ids: {}[]) {
