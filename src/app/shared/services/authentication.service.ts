b4a9e484f1dc1b6bf34da6cd601eadf5e52855aa
pierrelalin <pierre.lalin@isen.yncrea.fr>
2018-04-12 15:04:13 +0200

    Revert "2nd test avec request pour https"
    This reverts commit f9f86810600b8927cafba83b6494a913e4c6c898.

--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -65,7 +65,7 @@ export class AuthenticationService {
   };
 
   register(user): Observable<any> {
-    return request.post(environment.siteurl + '/api/register/', user)
+    return this.http.post(environment.siteurl + '/api/register/', user)
       .map(res => res.json())
       .catch(error => {
         return Observable.throw(error.json());
@@ -73,7 +73,7 @@ export class AuthenticationService {
   }
 
   login(user): Observable<any> {
-    return request.post(environment.siteurl+'/api/login/', user)
+    return this.http.post(environment.siteurl+'/api/login/', user)
       .map(res => res.json())
       .catch(error => {
         return Observable.throw(error.json());
